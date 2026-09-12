0113648ad5e100958e381bdd752b624909005b35
Leandro Cardoso <leandrocardosodev@gmail.com>
2026-09-12 14:52:54 -0300

    Base do frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,6 @@
+<template>
+  <main>
+    <h1>Leandro Cardoso</h1>
+    <p>Engenheiro de Software</p>
+  </main>
+</template>
---
+++ frontend/src/config/site.js
@@ -0,0 +1,10 @@
+export const siteConfig = {
+  name: 'Leandro Cardoso',
+  profession: 'Engenheiro de Software',
+  areas: [
+    'Performance',
+    'Inteligência Artificial',
+    'Automação',
+    'Cibersegurança',
+  ],
+}
